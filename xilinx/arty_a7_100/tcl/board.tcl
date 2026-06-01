50a3ecc14d7c384bbebe9d6004e64ae4f4175011
Loren Hung <lorenhung@berkeley.edu>
2026-06-01 13:12:43 -0700

    add: working Trenz Board FPGA Shell

--- xilinx/arty_a7_100/tcl/board.tcl
+++ xilinx/arty_a7_100/tcl/board.tcl
@@ -1,5 +1,5 @@
 # See LICENSE for license details.
 set name {arty-a7-100}
-set part_fpga {xc7a100ticsg324-1L}
-set part_board {digilentinc.com:arty-a7-100:part0:1.1}
+set part_fpga {xc7a100tfgg484-2}
+set part_board {}
 set bootrom_inst {rom}
